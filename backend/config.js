54ca0fb4df9a8b2746bc4becc37955b7628de0e7
Bhupendrakr12 <bhupendrapal2001@gmail.com>
2023-12-26 08:36:27 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://luckyaher47513:mU84y89lIRIIow2X@cluster0.da2m0yx.mongodb.net/booksm?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
